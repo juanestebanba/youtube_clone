b368fea4acd0a058bf47c1f6dbee49dfd9c5bacd
juanestebanba <juanestebanba@gmail.com>
2023-01-30 15:39:41 -0700

    Update fetchFromAPI.jsx

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -10,6 +10,8 @@ const options = {
     maxResults: 50,
   },
   headers: {
+    // This API key shouldn't be exposed here. It should be showed as a variable like process. 
+    // API-KEY and stored with the "secrets" feature of Replit, but the tool isn't working for some reason with this key. 
     'X-RapidAPI-Key': 'EDITED',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
